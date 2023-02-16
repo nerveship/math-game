2f7cd0ea0da48da72a6a2cccd3d626e096124505
John-Stuart Gonsales <nerveship@gmail.com>
2023-02-16 08:58:57 +0000

    added basic menu function/switch

--- Program.cs
+++ Program.cs
@@ -1,13 +1,47 @@
-﻿var date = DateTime.Now;
+﻿Console.WriteLine("Please type your name");
 
-string GetName()
+var date = DateTime.Now;
+string name = Console.ReadLine();
+
+MainMenu(date, name);
+
+static void MainMenu(DateTime date, string name)
 {
-    Console.WriteLine("Please type your name");
-    string name = Console.ReadLine();
-    return name;
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine($"Hello {name.ToUpper()}");
+    Console.WriteLine($"Today's date is {date}, I hope today is going well!");
+    Console.WriteLine("What game would you like to play today? Select an option from the numbers below:\n" +
+        "1. Addition\n" +
+        "2. Subtraction\n" +
+        "3. Multiplication\n" +
+        "4. Division\n" +
+        "5. Quit the program");
+    Console.WriteLine("-----------------------------------");
+    SelectGame();
 }
-
-void AdditionGame()
+static void SelectGame()
 {
-    break;
+    int gameSelected = Convert.ToInt32(Console.ReadLine());
+    switch (gameSelected)
+    {
+        case 0:
+            Console.WriteLine("Addition game selected");
+            break;
+
+        case 1:
+            Console.WriteLine("Subtraction game selected");
+            break;
+
+        case 2:
+            Console.WriteLine("Multiplication game selected");
+            break;
+
+        case 3:
+            Console.WriteLine("Division game selected");
+            break;
+
+        case 4:
+            Console.WriteLine("Quit selected");
+            break;
+    }
 }
