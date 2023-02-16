70d52b71bf6afb2494a08642209eff72f1e2ed13
John-Stuart Gonsales <nerveship@gmail.com>
2023-02-16 08:11:20 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿var date = DateTime.Now;
+
+string GetName()
+{
+    Console.WriteLine("Please type your name");
+    string name = Console.ReadLine();
+    return name;
+}
+
+void AdditionGame()
+{
+    break;
+}
